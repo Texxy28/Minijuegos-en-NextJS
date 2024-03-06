8b44fdaf35b4ef1ab07fe9c4e976605b50397833
Wilfredo Ortiz <willyortizlopezaeiou@gmail.com>
2024-03-06 16:29:06 -0500

    Definición básica de tetris

--- hooks/useDrawTetris.ts
+++ hooks/useDrawTetris.ts
@@ -23,18 +23,83 @@ export const useDrawTetris = () => {
 
         }
 
+        const piece = [
+            [
+                ["o", "o"],
+                ["o", "o"],
+            ],
+            [
+                ["o"],
+                ["o"],
+                ["o"],
+                ["o"],
+            ],
+            [
+                ["o", "x"],
+                ["o", "x"],
+                ["o", "o"],
+            ],
+            [
+                ["x", "o"],
+                ["x", "o"],
+                ["o", "o"],
+            ],
+            [
+                ["x", "o", "o"],
+                ["o", "o", "x"],
+            ],
+            [
+                ["o", "o", "x"],
+                ["x", "o", "o"],
+            ],
+            [
+                ["x", "o", "x"],
+                ["o", "o", "o"],
+            ],
+        ] 
+
+        const layout = [
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",],
+            ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x",]
+        ]
+
         const tiles = 20;
         const tileSize = context!.canvas.width / tiles;
 
-        let speed = 7;
+        let speed = 15;
+
+        let pieceX = 5;
+        let pieceY = 1;
 
         let timeout: ReturnType<typeof setTimeout>;
+
+        let randomPiece = piece[Math.floor(Math.random() * piece.length)];
 
         const draw = () => {
 
             clearScreen();
             divideScreen();
             drawGrid();
+            drawLayout();
+            drawPiece();
 
             timeout = setTimeout(draw, 1000/speed);
 
@@ -49,6 +114,32 @@ export const useDrawTetris = () => {
 
             for (let i = 1; i < 20; i++) {
                 context!.fillRect(tileSize*5, i*tileSize, tileSize*10, 1)
+            }
+
+        }
+
+        const drawLayout = () => {
+
+            context!.fillStyle = 'white'
+            for (let i = 0; i < layout.length; i++) {
+                for (let j = 0; j < layout[i].length; j++) {
+                    if (layout[i][j] === "o") {
+                        context!.fillRect(tileSize * (j+5), tileSize * (i), tileSize, tileSize)
+                    }
+                }
+            }
+
+        }
+
+        const drawPiece = () => {
+
+            context!.fillStyle = 'yellow'
+            for (let i = 0; i < randomPiece.length; i++) {
+                for (let j = 0; j < randomPiece[i].length; j++) {
+                    if (randomPiece[i][j] === "o") {
+                        context!.fillRect((pieceX+j+4)*tileSize, pieceY*i*tileSize, tileSize, tileSize)
+                    }
+                }
             }
 
         }
@@ -74,6 +165,24 @@ export const useDrawTetris = () => {
             context!.fillRect(tileSize*15, 0, tileSize * 5, tileSize * 5);
 
         }
+
+        const keyDown = (e: KeyboardEvent) => {
+
+            if (e.key === "c" || e.key === "C") {
+                randomPiece = piece[Math.floor(Math.random() * piece.length)];
+            }
+
+            if (e.key === "ArrowLeft") {
+                pieceX -= 1
+            }
+
+            if (e.key === "ArrowRight") {
+                pieceX += 1
+            }
+
+        }
+
+        document.body.addEventListener("keydown", (e: KeyboardEvent) => keyDown(e));
 
         draw();
 
